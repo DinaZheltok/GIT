f32042f073a7493dcc9e35876747f0165bcdd8e9
DianaZheltok <diana.zheltor.96@gmail.com>
2017-10-19 11:28:43 +0300

    Add comments

---
+++ DEV-9/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomReplacementOfElements
+{
+  /// <summary>
+  /// This is the entry point of the program.
+  /// </summary>
+	class EntryPoint
+	{
+    const string SOURCE = "12345678910";
+    const string DESTINATION = "qwertyuiop";
+
+    static void Main(string[] args)
+		{
+			RandomReplacer randomReplacer = new RandomReplacer(SOURCE, DESTINATION);
+			string result = randomReplacer.RandomReplace();
+			Console.Write(SOURCE + "\n" + DESTINATION + "\n" + result);
+			Console.ReadKey();
+		}
+	}
+}
--- DEV-9/RandomReplacer.cs
+++ DEV-9/RandomReplacer.cs
@@ -2,36 +2,52 @@
 
 namespace RandomReplacementOfElements
 {
+  /// <summary>
+  /// This class is used to randomly replace elements from one string to another.
+  /// </summary>
 	class RandomReplacer
 	{
 		public string SourceString { get; set; }
 		public string DestinationString { get; set; }
 		Random rand = new Random();
+    
 		public RandomReplacer(string sourceString,string destinationString)
 		{
 			SourceString = sourceString;
 			DestinationString = destinationString;
 		}
+
 		public int SearchIndexFromStringSource()
 		{
 			return rand.Next(SourceString.Length);
 		}
+
 		public int SearchIndexFromStringDestination()
 		{
 			return rand.Next(DestinationString.Length);
 		}
+
 		public int GetLengthOfPieceFromStringSource(int sourceIndex)
 		{
 			return rand.Next(1, SourceString.Length - sourceIndex);
 		}
+
 		public int GetLengthOfPieceFromStringDestination( int destinationIndex)
 		{
 			return rand.Next(1, DestinationString.Length - destinationIndex);
 		}
+
 		public string GetPieceFromString( int sourceIndex, int sizeOfPiece)
 		{
 			return SourceString.Substring(sourceIndex, sizeOfPiece);
 		}
+
+    /// <summary>
+    /// Using the methods implemented above,this method is used to randomly replace elements from one string to another.
+    /// </summary>
+    /// <returns>
+    /// This method return new string with with a randomly-replaced sequence of elements.
+    /// </returns>
 		public string RandomReplace()
 		{
 			int startIndexSource = SearchIndexFromStringSource();
