3daf497d67b3e1b2e700e5b1b9885bd2d918f3ce
DianaZheltok <diana.zheltor.96@gmail.com>
2017-10-19 10:53:33 +0300

    Add DEV-9: The random replacement of the elements.

---
+++ DEV-9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RandomReplacementOfElements
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string source = "12345678910";
+			string destination = "qwertyuiop";
+
+			RandomReplacer randomReplacer = new RandomReplacer(source, destination);
+			string result = randomReplacer.RandomReplace();
+			Console.Write(source + "\n" + destination + "\n" + result);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ DEV-9/RandomReplacer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RandomReplacementOfElements
+{
+	class RandomReplacer
+	{
+		public string SourceString { get; set; }
+		public string DestinationString { get; set; }
+		Random rand = new Random();
+		public RandomReplacer(string sourceString,string destinationString)
+		{
+			SourceString = sourceString;
+			DestinationString = destinationString;
+		}
+		public int SearchIndexFromStringSource()
+		{
+			return rand.Next(SourceString.Length);
+		}
+		public int SearchIndexFromStringDestination()
+		{
+			return rand.Next(DestinationString.Length);
+		}
+		public int GetLengthOfPieceFromStringSource(int sourceIndex)
+		{
+			return rand.Next(1, SourceString.Length - sourceIndex);
+		}
+		public int GetLengthOfPieceFromStringDestination( int destinationIndex)
+		{
+			return rand.Next(1, DestinationString.Length - destinationIndex);
+		}
+		public string GetPieceFromString( int sourceIndex, int sizeOfPiece)
+		{
+			return SourceString.Substring(sourceIndex, sizeOfPiece);
+		}
+		public string RandomReplace()
+		{
+			int startIndexSource = SearchIndexFromStringSource();
+			int sizeOfPieceFromStringSource = GetLengthOfPieceFromStringSource(startIndexSource);
+			string pieceFromStringSource = GetPieceFromString(startIndexSource, sizeOfPieceFromStringSource);
+			int startIndexDestination = SearchIndexFromStringDestination();
+			int sizeOfPieceFromStringDestination = GetLengthOfPieceFromStringDestination(startIndexDestination);
+			string result = DestinationString.Remove(startIndexDestination, sizeOfPieceFromStringDestination);
+			result = result.Insert(startIndexDestination, pieceFromStringSource);
+			return result;
+		}
+	}
+}
