844801a25c11d2a8254a279ecc4424c55d69db3e
DianaZheltok <diana.zheltor.96@gmail.com>
2017-10-26 11:34:36 +0300

    Add task: Search for the maximum common substring

---
+++ SearchForTheMaximumSubstring/SearchForTheMaximumSubstring/RandomFillingOfStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SearchForTheMaximumSubstring
+{
+  /// <summary>
+  /// Сlass, that contains a method for randomly filling strings.
+  /// </summary>
+  class RandomFillingOfStrings
+  {
+    Random rand = new Random();
+    public string RandomFillingString()
+    {
+      string alphabet = "abcdefghijklmnopqrstuvwxyz";
+      int length = 40;
+      char[] randomArray = new char[length];
+      for (int i = 0; i < length; i++)
+      {
+        int randIndex = rand.Next(alphabet.Length);
+        randomArray[i] = alphabet[randIndex];
+      }
+      string randomString = new string(randomArray);
+      return randomString;
+    }
+  }
+}
